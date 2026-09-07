73f2d800fd1f7ed54f0171a6f21d4fa523284fe0
poojaiiit17 <poojaiiit17@gmail.com>
2026-09-07 16:38:49 +0530

    Fix Spring component scanning for controllers

--- src/main/java/com/zomato/main/ZomatoApplication.java
+++ src/main/java/com/zomato/main/ZomatoApplication.java
@@ -3,7 +3,7 @@ package com.zomato.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.zomato")
 public class ZomatoApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZomatoApplication.class, args);
